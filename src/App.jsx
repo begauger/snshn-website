0bdeda8ab7d424ac43177e155dd47a6b63d607ee
Ben Gauger <begauger00@gmail.com>
2025-12-14 19:54:12 -0600

    scrolling

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import Blog from './pages/Blog';
@@ -16,6 +17,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
